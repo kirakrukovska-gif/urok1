3f420b298e95d24b8e5ab5ab65dcfbd05c5ec0b6
kirakrukovska-gif <kirakrukovska@gmail.com>
2025-09-26 20:15:48 +0300

    lesson2.py

--- lesson.py
+++ lesson.py
@@ -1,1 +1,11 @@
-print("hello")
+class Student:
+    #height=170
+    def __init__(self):
+        print(self.height)
+        self.height = 230
+    height = 200
+    def printer(self):
+        print(self.height)
+
+Ira = Student()
+Ira.printer()
